b82903952535229cee1e6f27ba75f5ab938870e5
Uliana Bakhmat <uljanaiwanowa@gmail.com>
2020-09-01 22:35:26 +0300

    added miles

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String [] args) {
+        int ticket_price = 25000; //one ticket price
+        int bonus_miles = 20;// for each miles
+        int total_miles = ticket_price / bonus_miles; // total miles for buying onerc ticket
+        System.out.println(total_miles);
+
+    }
+}
