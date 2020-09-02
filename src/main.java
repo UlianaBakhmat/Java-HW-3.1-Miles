aab9ca53fa00d80356ccb1460fa86fdee0a0fb7a
UlianaBakhmat <60362431+UlianaBakhmat@users.noreply.github.com>
2020-09-02 14:35:53 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,8 +1,8 @@
-public class main {
-    public static void main(String [] args) {
-        int ticket_price = 25000; //one ticket price
-        int bonus_miles = 20;// for each miles
-        int total_miles = ticket_price / bonus_miles; // total miles for buying onerc ticket
+public class Main {
+    public static void Main(String [] args) {
+        int ticketPrice = 25000; //one ticket price
+        int bonusMiles = 20;// for each miles
+        int totalMiles = ticket_price / bonus_miles; // total miles for buying onerc ticket
         System.out.println(total_miles);
 
     }
